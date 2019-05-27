3e39f6ee80fb52d4d81c86cb4646f3be243bbf61
alex <work@xingyongtao.xyz>
2019-05-27 11:44:52 +0800

    Update:将基本接口完成

--- task.py
+++ task.py
@@ -11,10 +11,18 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient('127.0.0.1', 27017)
 
-skill_db = client.opentask.skill
+skill_db = client.open_task.skill
 user_db = client.open_task.user
 
-
+def new_line(db_name):
+    dic = {}
+    up_no = redis_db.incr(db_name + "_number")
+    timestamp = int(time.time())
+    dic["id"] = up_no
+    dic["create_time"] = timestamp
+    dic["update_time"] = timestamp
+    dic['state'] = True
+    return dic
 
 
 redis_db = redis.StrictRedis(host='localhost', port=6379)
@@ -28,49 +36,56 @@ def insert_cover(db_name, dic):
     return dic
 
 # 技能列表展示页
-@app.route("/v1/list_skill",methods=["POST"])
-def list_skill():
-	skills = skill_db.find({"state":True}).sort("count",-1)
-	return jsonify({"state":True,"skills":skills})
+@app.route("/skill/list_skills",methods=["POST"])
+def list_skills():
+    skills = list(skill_db.find({"state":True},{"_id":False}).sort("count",-1))
+    return jsonify({"state":True,"skills":skills})
 
-@app.route("/get_users",methods=["GET","POST"])
+@app.route("/skill/skills_get_users",methods=["GET","POST"])
 def get_users():
-	skill = request.json.get("skill")
-	user_list = list(user_db.find({"skills.name":skill},{"_id":False}))
-	# 这里缺少历史记录
-	return jsonify({"state":True})
-	
+    skill = request.json.get("skill")
+    user_list = list(user_db.find({"skill":skill},{"_id":False}))
+    # 这里缺少历史记录
+    return jsonify({"state":True,'user_list':user_list})
+    
 # 用户设置页面
-@app.route("/update_user_info",methods=["GET","POST"])
+@app.route("/skill/update_user_info",methods=["GET","POST"])
 def update_user_info():
-	address = request.json.get('address')
-	info = request.json.get('info')
-	user_info = user_db.find_one({'address':address})
-	if user_info:
-		skills_old = [x['name'] for x in user_info['skills']]
-		skills_new = info['skills']
-		skill_add = [x for x in skills_new if x not in skill_old]
-		skill_del = [x for x in skills_old if x not in skill_new]
-		skill_db.update_many({'name':{"$in":skill_del}},{"$inc":{"count",-1}})
-		for name in skill_add:
-			update_info = skill_db.update_one({'name':name},{"$inc":{"count",1}})
-			if not update_info['nModified']:
-				info = insert_cover("task_skill",{
-						"name":name,
-						'count':1
-					})
+    address = request.json.get('address')
+    print address
+    skills = request.json.get('skill')
+    email = request.json['email']
+    user_info = user_db.find_one({'address':address})
+    if skills:
+        if user_info:
+            user_skill_in_db = user_info['skill']
+        else:
+            user_skill_in_db = []
+        skill_old = [x for x in user_skill_in_db]
+        skill_new = skills
+        skill_add = [x for x in skill_new if x not in skill_old]
+        skill_del = [x for x in skill_old if x not in skill_new]
+        print skill_del
+        skill_db.update_many({'name':{"$in":skill_del}},{"$inc":{"count":-1}})
+        for name in skill_add:
+            update_info = skill_db.update_one({'name':name},{"$setOnInsert":new_line('task_skill'),"$inc":{"count":1}},upsert=True)
+            
 
-	user_db.update({'address':address},{"$set":info,"$setOnInsert":info})
-	return jsonify({"state":True})
+    user_db.update({'address':address},{"$set":{"skill":skills,'email':email},'$setOnInsert':dict(new_line('task_user').items() + {"address":address}.items())},upsert=True)
+    return jsonify({"state":True})
 
-@app.route("/get_user_info",methods=["GET","POST"])
+
+    
+
+
+@app.route("/skill/get_user_info",methods=["GET","POST"])
 def get_user_info():
-	address = request.json.get('address')
-	user_info = user_db.find_one({"address":address},{"_id":False})
-	return jsonify({"state":True})
+    address = request.json.get('address')
+    user_info = user_db.find_one({"address":address},{"_id":False})
+    return jsonify({"state":True,"user_info":user_info})
 
 
-	
+    
 
 
 
@@ -81,4 +96,4 @@ def get_user_info():
 
 
 if __name__ == "__main__":
-	app.run(debug=True,port=5236,host="0.0.0.0")
+    app.run(debug=True,port=5236,host="0.0.0.0")
