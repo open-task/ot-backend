87afec1c8f641f9f2b5c61425945ac865ad60f42
alex <work@xingyongtao.xyz>
2019-06-14 16:04:28 +0800

    Update:成熟版,用来迁移

--- task.py
+++ task.py
@@ -5,10 +5,6 @@ from werkzeug import secure_filename
 import os
 from testModel import *
 
-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 app = Flask(__name__)
 
 client = pymongo.MongoClient('127.0.0.1', 27017)
@@ -38,9 +34,9 @@ def insert_cover(db_name, dic):
     return dic
 
 # 技能列表展示页
-@app.route("/skill/list_skills",methods=["POST"])
+@app.route("/skill/list_skills",methods=["GET","POST"])
 def list_skills():
-    skills = list(skill_db.find({"state":True},{"_id":False}).sort("count",-1))
+    skills = list(skill_db.find({"state":True,"count":{"$gt":0}},{"_id":False}).sort("count",-1))
     return jsonify({"state":True,"skills":skills})
 
 @app.route("/skill/skills_get_users",methods=["GET","POST"])
@@ -58,7 +54,7 @@ def get_users():
 @app.route("/skill/update_user_info",methods=["GET","POST"])
 def update_user_info():
     address = request.json.get('address')
-    print address
+    print(address)
     skills = request.json.get('skill')
     email = request.json['email']
     user_info = user_db.find_one({'address':address})
@@ -71,13 +67,14 @@ def update_user_info():
         skill_new = skills
         skill_add = [x for x in skill_new if x not in skill_old]
         skill_del = [x for x in skill_old if x not in skill_new]
-        print skill_del
+        print(skill_del)
         skill_db.update_many({'name':{"$in":skill_del}},{"$inc":{"count":-1}})
         for name in skill_add:
             update_info = skill_db.update_one({'name':name},{"$setOnInsert":new_line('task_skill'),"$inc":{"count":1}},upsert=True)
             
-
-    user_db.update({'address':address},{"$set":{"skill":skills,'email':email},'$setOnInsert':dict(new_line('task_user').items() + {"address":address}.items())},upsert=True)
+    oninsert = new_line('task_user').copy()
+    oninsert.update({"address":address})
+    user_db.update({'address':address},{"$set":{"skill":skills,'email':email},'$setOnInsert':oninsert},upsert=True)
     return jsonify({"state":True})
 
 
@@ -102,4 +99,4 @@ def get_user_info():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5236,host="0.0.0.0")
+    app.run(debug=True,port=5236)
